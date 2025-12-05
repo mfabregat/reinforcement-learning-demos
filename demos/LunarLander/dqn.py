e1b08548f1a6575adcd9a5e289956a8cc3d6cd14
FAM1MOA <fam1moa@bosch.com>
2025-12-05 12:46:18 +0000

    started dqn

--- demos/LunarLander/dqn.py
+++ demos/LunarLander/dqn.py
@@ -1,2 +1,43 @@
 import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+env = gym.make("LunarLander-v3")
+
+seed = 42
+random.seed(seed)
+torch.manual_seed(seed)
+env.reset(seed=seed)
+env.action_space.seed(seed)
+env.observation_space.seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+
+
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
